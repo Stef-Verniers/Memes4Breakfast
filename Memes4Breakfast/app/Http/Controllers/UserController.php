64bbe56eb71dbf42a73e5783588c7728a1879b18
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2024-07-23 00:15:21 +0200

    Ready to create users

--- Memes4Breakfast/app/Http/Controllers/UserController.php
+++ Memes4Breakfast/app/Http/Controllers/UserController.php
@@ -7,10 +7,16 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    // Get all users
     public function index() {
         $users = User::all();
         return view('home', [
             'users' => $users
         ]);
+    }
+
+    // Create a new user
+    public function create(Request $r) {
+        // COMING SOON
     }
 }
